c981c9e1507f8732f051afd9209de0ddd7f681ff
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-09-07 16:02:04 +0200

    starting things out

--- glox/lox/exprVisitor.go
+++
@@ -1,9 +0,0 @@
-package lox
-
-type exprVisitor interface {
-	visitBinary(eb *ExprBinary) interface{}
-	visitGrouping(eg *ExprGrouping) interface{}
-	visitLiteral(el *ExprLiteral) interface{}
-	visitUnary(eu *ExprUnary) interface{}
-}
-
