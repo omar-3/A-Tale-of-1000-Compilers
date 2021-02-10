1380f48797438ee2019dd2796ef2fb3b0339e475
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2021-02-10 14:03:32 +0200

    starting c sharp pascal

---
+++ pascalCsharp/pascalCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pascalCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
